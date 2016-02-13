9388a972874f78fe1aea89abb715f38e8e1a3e1e
Duncan Nicoll <dunknicoll@Duncans-MacBook-Pro.local>
2016-02-13 00:08:49 +0000

    initial code

---
+++ src/clock.js
@@ -0,0 +1,39 @@
+import 'babel-polyfill';
+import config from 'config';
+import Twitter from 'twitter';
+import moment from 'moment';
+import 'moment-timezone';
+
+function announce () {
+    let hourCheck = moment().tz("Europe/London").format('HH');
+    let minuteCheck = moment().tz("Europe/London").format('mm');
+    let timeCheck = hourCheck + ":" + minuteCheck;
+    let dayCheck = moment().tz("Europe/London").day();
+
+    if (dayCheck != 0 && dayCheck != 6) {
+
+        if(timeCheck=="09:15"){
+            sendMessage("Nice of you to join us");
+        }
+
+        if(timeCheck=="17:00" && dayCheck==5) {
+            sendMessage("Down tools lads");
+        }
+    }
+
+    if(minuteCheck=="00"){
+        let hour = moment().tz("Europe/London").format('h');
+        let bangString = Array.from({length: hour}, x => "bang!").join(" "); 
+        sendMessage(bangString);
+    }
+}
+
+function sendMessage(message) {
+    let twitterConfig = config.get("twitter");
+    let client = new Twitter(twitterConfig);
+    client.post('statuses/update', {status: message},  (error, tweet, response) => {
+      if(error) throw error;
+    });
+}
+
+announce();
